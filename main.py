6bbbae8cece3f4b32007b65f29b08c401ca60454
Amogh <axm0754@mavs.uta.edu>
2023-10-06 20:48:43 -0500

    refactor: updated imports

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from ui_utils import App
-import settings
-
+from ui import App
+from utils import settings
 
 if __name__ == "__main__":
     settings.init()
 
     app = App()
-    app.after(5, app.tab_press_check, app.tab_press_check) # Starting the tab check
-    app.mainloop() # Starting the app
+    app.after(5, app.tab_press_check, app.tab_press_check)  # Starting the tab check
+    app.mainloop()                                          # Starting the app
 
 
