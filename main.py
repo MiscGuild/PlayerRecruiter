15d615c966476c566dac40d12bbf014399e90bd8
Amogh <axm0754@mavs.uta.edu>
2023-10-06 23:24:47 -0500

    feat: automatically checks if the api key is valid everytime the program is started.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from ui import App
 from utils import settings
+from asyncio import run
+from utils.request_utils import check_api_key
 
 if __name__ == "__main__":
     settings.init()
+    settings.setAPIKeyValidity(run(check_api_key()))
 
     app = App()
     app.after(5, app.tab_press_check, app.tab_press_check)  # Starting the tab check
--- utils/settings.py
+++ utils/settings.py
@@ -33,6 +33,10 @@ def init():
     NETWORK_LEVEL = config["REQUIREMENTS"]["NETWORK_LEVEL"]
     globals().update(locals())
 
+def setAPIKeyValidity(apiKeyValidity):
+    API_KEY_VALID = apiKeyValidity
+    globals().update(locals())
+
 
 def getSettings():
     user_home = os.path.expanduser("~")
@@ -80,25 +84,22 @@ def updateSettings(API_KEY, PATH, SKYWARS_WINS, SKYWARS_LEVEL, BEDWARS_STARS, BE
                    DUELS_WINS, DUELS_WLR, ARCADE_WINS, MURDER_MYSTERY_WINS, MINIMUM_GEXP,
                    MAXIMUM_GUILD_LEVEL, NETWORK_LEVEL):
     user_home = os.path.expanduser("~")
-    User_Profile = os.getenv('USERPROFILE')
     documents_path = os.path.join(user_home, 'Documents')
     folder_name = "MiscRecruitment"
     folder_path = os.path.join(documents_path, folder_name)
     file_name = "config.json"
 
-    paths_dict = {
-        "Lunar Client": fr'{User_Profile}\.lunarclient\offline\multiver\logs\latest.log',
-        "Badlion Client": fr'{User_Profile}\AppData\Roaming\.minecraft\logs\blclient\minecraft\latest.log',
-        "Feather Client": fr'{User_Profile}\AppData\Roaming\.minecraft\logs\latest.log',
-        "Vanilla": fr'{User_Profile}\AppData\Roaming\.minecraft\logs\latest.log'
-    }
+
 
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
 
+    if API_KEY == "HIDDEN":
+        API_KEY = globals()["API_KEY"]
+
     json_data = {
         "API_KEY": API_KEY,
-        "PATH": paths_dict.get(PATH),
+        "PATH": globals()["paths_dict"].get(PATH),
         "REQUIREMENTS": {
             "SKYWARS_WINS": SKYWARS_WINS,
             "SKYWARS_LEVEL": SKYWARS_LEVEL,
